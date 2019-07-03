1d7d55960ddf9ce1ac11e88090be7ac60315f1f1
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2019-07-03 17:52:03 -0300

    Add the system_call_2 function and its macro rules

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,20 @@
 #![feature(asm)]
 
 #[cfg(target_arch = "x86_64")]
+pub unsafe fn system_call_2(number: usize, _1: usize, _2: usize) -> usize {
+    let mut return_value;
+
+    asm!("syscall"
+       : "={rax}" (return_value)
+       : "{rax}" (number), "{rdi}" (_1), "{rsi}" (_2)
+       : "rcx", "r8", "r9", "r10" "r11", "cc", "memory"
+       : "volatile"
+       );
+
+    return_value
+}
+
+#[cfg(target_arch = "x86_64")]
 pub unsafe fn system_call_3(number: usize, _1: usize, _2: usize, _3: usize) -> usize {
     let mut return_value;
 
@@ -58,6 +72,10 @@ pub unsafe fn system_call_6(number: usize, _1: usize, _2: usize, _3: usize, _4: 
 }
 
 macro_rules! system_call {
+    ($number:expr, $_1:expr, $_2:expr) => (
+        system_call_2($number, $_1, $_2);
+    );
+
     ($number:expr, $_1:expr, $_2:expr, $_3:expr) => (
         system_call_3($number, $_1, $_2, $_3);
     );
