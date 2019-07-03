8bb911c31d695b5dc6df553ae18cee05ea2e8569
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2019-07-03 20:10:46 -0300

    Define module that maps to the target architecture
    Allows generic references to the targeted computer architecture.

--- src/lib.rs
+++ src/lib.rs
@@ -7,30 +7,30 @@ pub mod architecture;
 #[macro_export]
 macro_rules! system_call {
     ($number:expr) => (
-        ::linux::architecture::x86_64::system_call_0($number);
+        ::linux::architecture::target::system_call_0($number);
     );
 
     ($number:expr, $_1:expr) => (
-        ::linux::architecture::x86_64::system_call_1($number, $_1);
+        ::linux::architecture::target::system_call_1($number, $_1);
     );
 
     ($number:expr, $_1:expr, $_2:expr) => (
-        ::linux::architecture::x86_64::system_call_2($number, $_1, $_2);
+        ::linux::architecture::target::system_call_2($number, $_1, $_2);
     );
 
     ($number:expr, $_1:expr, $_2:expr, $_3:expr) => (
-        ::linux::architecture::x86_64::system_call_3($number, $_1, $_2, $_3);
+        ::linux::architecture::target::system_call_3($number, $_1, $_2, $_3);
     );
 
     ($number:expr, $_1:expr, $_2:expr, $_3:expr, $_4:expr) => (
-        ::linux::architecture::x86_64::system_call_4($number, $_1, $_2, $_3, $_4);
+        ::linux::architecture::target::system_call_4($number, $_1, $_2, $_3, $_4);
     );
 
     ($number:expr, $_1:expr, $_2:expr, $_3:expr, $_4:expr, $_5:expr) => (
-        ::linux::architecture::x86_64::system_call_5($number, $_1, $_2, $_3, $_4, $_5);
+        ::linux::architecture::target::system_call_5($number, $_1, $_2, $_3, $_4, $_5);
     );
 
     ($number:expr, $_1:expr, $_2:expr, $_3:expr, $_4:expr, $_5:expr, $_6:expr) => (
-        ::linux::architecture::x86_64::system_call_6($number, $_1, $_2, $_3, $_4, $_5, $_6);
+        ::linux::architecture::target::system_call_6($number, $_1, $_2, $_3, $_4, $_5, $_6);
     );
 }
