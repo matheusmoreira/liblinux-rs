b40c7fc63d41928288d8d94499b9b8835ed36616
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2019-07-03 17:42:42 -0300

    Define the rules for the system_call macro
    This macro dispatches a function invocation with 6 arguments
    to the system_call_6 function.

--- src/lib.rs
+++ src/lib.rs
@@ -14,3 +14,9 @@ pub unsafe fn system_call_6(number: usize, _1: usize, _2: usize, _3: usize, _4: 
 
     return_value
 }
+
+macro_rules! system_call {
+    ($number:expr, $_1:expr, $_2:expr, $_3:expr, $_4:expr, $_5:expr, $_6:expr) => (
+        system_call_6($number, $_1, $_2, $_3, $_4, $_5, $_6);
+    );
+}
