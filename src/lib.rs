258a0d12c1b058480ec42e1306ba3563d909eabb
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2019-07-03 17:47:06 -0300

    Add the system_call_5 function and its macro rules

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,20 @@
 #![feature(asm)]
 
 #[cfg(target_arch = "x86_64")]
+pub unsafe fn system_call_5(number: usize, _1: usize, _2: usize, _3: usize, _4: usize, _5: usize) -> usize {
+    let mut return_value;
+
+    asm!("syscall"
+       : "={rax}" (return_value)
+       : "{rax}" (number), "{rdi}" (_1), "{rsi}" (_2), "{rdx}" (_3), "{r10}" (_4), "{r8}" (_5)
+       : "rcx", "r8", "r9", "r10" "r11", "cc", "memory"
+       : "volatile"
+       );
+
+    return_value
+}
+
+#[cfg(target_arch = "x86_64")]
 pub unsafe fn system_call_6(number: usize, _1: usize, _2: usize, _3: usize, _4: usize, _5: usize, _6: usize) -> usize {
     let mut return_value;
 
@@ -16,6 +30,10 @@ pub unsafe fn system_call_6(number: usize, _1: usize, _2: usize, _3: usize, _4: 
 }
 
 macro_rules! system_call {
+    ($number:expr, $_1:expr, $_2:expr, $_3:expr, $_4:expr, $_5:expr) => (
+        system_call_5($number, $_1, $_2, $_3, $_4, $_5);
+    );
+
     ($number:expr, $_1:expr, $_2:expr, $_3:expr, $_4:expr, $_5:expr, $_6:expr) => (
         system_call_6($number, $_1, $_2, $_3, $_4, $_5, $_6);
     );
